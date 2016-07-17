57cdba7dedd6ae45f1ae34d15d59a61311e095aa
reptileinx <reptileinx@email.com>
2016-07-18 09:58:35 +1000

    added some logging for error calls

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-  var express, bodyparser, port, server, bookrouter, app, Book, db, mongoose, url, connection_string;
+  var express, bodyparser, port, server, bookrouter,  
+      app, Book, db, mongoose, url, connection_string, accessLogStream,
+      fs, morgan, path;
 
+  fs = require('fs');
+  morgan = require('morgan');
+  path = require('path');
   express = require('express');
   Book = require('./models/bookmodel');
   bookrouter = require('./routes/bookroutes')(Book);
   bodyparser = require('body-parser');
+  mongoose = require('mongoose');
+  
   app = express();
   port = process.env.PORT || 3000;
   connection_string = process.env.CONN || 'mongodb://localhost/bookapi';
-  mongoose = require('mongoose');
   db = mongoose.connect(connection_string);    
+  // create a write stream (in append mode)
+  accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+  
   app.use(bodyparser.urlencoded({ extended: true }));
   app.use(bodyparser.json());
   app.use('/api/books', bookrouter);
+  // setup the logger
+  app.use(morgan('combined', {stream: accessLogStream}));
+
   server = app.listen(port, function () {
     console.log('App is running my server on PORT: ' + port);
   });
