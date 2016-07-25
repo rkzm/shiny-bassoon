19f27ab031da36204969c941796fda972d18788c
reptileinx <reptileinx@email.com>
2016-07-25 22:01:34 +1000

    added unit tests using sinon and should

--- controllers/bookcontroller.js
+++ controllers/bookcontroller.js
@@ -1,8 +1,14 @@
 var bookcontroller = function (Book) {
     var post = function (req, res) {
         book = new Book(req.body);
-        book.save(); 
-        res.status(201).send(book);
+        if (!req.body.title) {
+            res.status(400);
+            res.send('Title is required');
+        } else {
+            book.save();
+            res.status(201);
+            res.send(book);
+        }
     };
 
     var get = function (req, res) {
