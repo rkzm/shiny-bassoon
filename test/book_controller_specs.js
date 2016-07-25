601f3c6c8f898ad2be447b00075ab89a36df28b9
reptileinx <reptileinx@email.com>
2016-07-25 22:18:22 +1000

    fixed the require statement to make tests pass

--- test/book_controller_specs.js
+++ test/book_controller_specs.js
@@ -18,10 +18,10 @@ describe('Book Controller Tests:', function (){
 
             var res = {
                 status: sinon.spy(),
-                   send: sinon.spy()
+                send: sinon.spy()
             };
 
-            var bookController = require('../controllers/bookController')(Book);
+            var bookController = require('../controllers/bookcontroller')(Book);
             bookController.post(req, res);
             res.status.calledWith(400).should.equal(true, 'Bad Status' + res.status.args[0][0]);
             res.send.calledWith('Title is required').should.equal(true);
